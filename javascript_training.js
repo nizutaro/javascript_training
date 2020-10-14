26106dfbc1c5779736d1242af9444f9cab51ec6e
nizutaro <niduka81@gmail.com>
2020-10-14 22:59:28 +0900

    first commit

---
+++ javascript_training.js
@@ -0,0 +1,24 @@
+// 比較演算子
+const password = "hoge";
+
+//等しい場合
+if(password === "hoge"){
+  console.log("等しい");
+}
+
+//等しくない場合
+if(password !== "gyohe"){
+  console.log("等しくない");
+}
+
+//switch文 VBAのselect case
+
+// switch(条件の値){
+// case 値1:
+// 「条件の値」が「値1」と等しい時の処理
+// break;
+
+//default:・・・どれも正しくない場合
+//処理
+//break
+//}
